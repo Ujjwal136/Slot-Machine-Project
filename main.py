6c8602d22dc1338c431193323ef44b3df266331f
Ujjwal Garg <gargujjwal136@gmail.com>
2025-08-15 22:30:34 +0530

    Main Fun

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 
 import random
 
-
 MAX_ROWS = 3
 MAX_COLS = 3
 MAX_BETS = 3
 MIN_BETS = 1
-
 
 symbols_count = {
     "@": 2,
@@ -30,7 +28,7 @@ def get_slot_machine_spin(rows,cols,symbols):
             value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
-        columns.appen(column)
+        columns.append(column)
 
 def deposit():
     
@@ -41,30 +39,49 @@ def deposit():
             if amount >= 500:
                 break
             else: 
-                print("Amount must be greater than $500")
+                print("Amount must be greater than $500 ")
         else:
-            print("Enter the Valid Amount!")
+            print("Enter the Valid Amount! ")
+    return amount
 
 def lines():
     while True:
-        lines = input(f"Enter the Number of Lines you want to Bet on between {MIN_BETS}-{MAX_BETS}")
+        lines = input(f"Enter the Number of Lines you want to Bet on between {MIN_BETS}-{MAX_BETS} ")
         if lines.isdigit():
             lines = int(lines)
             if MAX_BETS >= lines >= MIN_BETS:
                 break
             else:
-                print("Please Enter the Correct Amount of Lines you want to Bet on!")
+                print("Please Enter the Correct Amount of Lines you want to Bet on! ")
         else:
-            print("Please Enter Number of lines only!")
+            print("Please Enter Number of lines only! ")
+    return lines
 
 def bet_amount():
     while True:
-        bets = input(f"Enter the Amount of bet on each line!")
+        bets = input(f"Enter the Amount of bet on each line! ")
         if bets.isdigit():
             bets = int(bets)
             if 1 <= bets <= 100:
                 break
             else:
-                print("Please Enter the Amount between [1 to 100]")
+                print("Please Enter the Amount between [$1 to $100] ")
         else:
-            print("Please Enter valid number!")
+            print("Please Enter valid Amount! ")
+    return bets
+
+def main():
+    balance = deposit()
+    line = lines()
+    print(f"Total Amount you deposited is ${balance}")
+    print(f"Number of Lines you choose to Bet are {line}")
+    while True:
+        bet = bet_amount()
+        if (bet*line) <= balance:
+            print(f"Your Total Bet is ${bet * line}")
+            break
+        else :
+            print("Your Betting amount is Larger than Your Deposit!")
+            break
+
+main()
