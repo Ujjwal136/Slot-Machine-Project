c5a2ba16bc83430850d73219199ae6b4f6c1b8a6
Ujjwal Garg <gargujjwal136@gmail.com>
2025-08-16 15:11:21 +0530

    Removing The Logic Func

--- main.py
+++ main.py
@@ -7,29 +7,6 @@ MAX_COLS = 3
 MAX_BETS = 3
 MIN_BETS = 1
 
-symbols_count = {
-    "@": 2,
-    "$": 1,
-    "&": 6,
-    "#": 5
-}
-
-def get_slot_machine_spin(rows,cols,symbols):
-    all_symbols = []
-    for symbol, symbol_count in range(symbols_count):
-        for _ in range(symbol_count):
-            all_symbols.append("symbol")
-    
-    columns = []
-    for _ in range(cols):
-        column = []
-        current_symbols = all_symbols[:]
-        for _ in range(rows):
-            value = random.choice(current_symbols)
-            current_symbols.remove(value)
-            column.append(value)
-        columns.append(column)
-
 def deposit():
     
     while True:
